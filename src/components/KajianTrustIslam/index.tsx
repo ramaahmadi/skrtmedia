07845d3ac90455a30b3d749c4533f3caa04290a4
Rama Ahmadi <67901561+ramaahmadi@users.noreply.github.com>
2026-05-13 04:15:46 +0000

    Fix JSX header mismatch in KajianTrustIslam and restore build

--- src/components/KajianTrustIslam/index.tsx
+++ src/components/KajianTrustIslam/index.tsx
@@ -333,7 +333,7 @@ export default function KajianTrustIslam() {
               </Link>
             </nav>
           </div>
-        </header>
+        </div>
 
         {/* Mobile Navigation */}
         <AnimatePresence>
